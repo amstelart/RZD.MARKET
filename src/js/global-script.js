104d528ba4f5327547a79aa8834cbc8159a6e411
Артем <amstelart@MacBook-Pro-Artem.local>
2018-03-04 20:26:53 +0300

    main

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -1,7 +1,36 @@
 // Если на проекте jQuery
-// $( document ).ready(function() {
-//   // code
-// });
+$( document ).ready(function() {
+  $(".product-carousel").owlCarousel({
+    items: 3,
+    nav: true,
+    loop: true,
+    margin: 30,
+    center: false,
+    responsive : {
+      0 : {
+        items: 1,
+        nav: true,
+        loop: true,
+        center: false,
+      },
+      480 : {
+        items: 2,
+      },
+      768 : {
+        items: 3,
+      },
+      992 : {
+        items: 4,
+      },
+      1200 : {
+        items: 4,
+      },
+      1800 : {
+        items: 4,
+      }
+    }
+  });
+});
 
 // Изоляция без jQuery
 // (function(){
