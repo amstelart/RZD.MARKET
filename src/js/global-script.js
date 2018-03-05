8cc081c9885b0106bbd919088c811c2126c60f59
Артем <amstelart@MacBook-Pro-Artem.local>
2018-03-05 18:39:10 +0300

    add main feedback block

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -34,6 +34,7 @@ $( document ).ready(function() {
     items: 1,
     nav: true,
     loop: true,
+    autoHeight: true,
     center: true,
     dots: false,
     navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 2px;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 2px;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>']
